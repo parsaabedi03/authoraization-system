0c6e15c2d821fe379d7241c97d9947599cc9454c
Parsa Abedi <parsaab03@gmail.com>
2026-05-17 00:13:03 +0330

    feat: connect to mongoose

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const { notFoundError, errorHandler } = require("./utils/error-handling");
 const { AllRouters } = require("./router/index.routes");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost:27017/authorization")
+  .then(console.log("Connected to mongodb"))
+  .catch((err) => console.log(err.message));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
