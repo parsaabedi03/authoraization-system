603ba437ca4bb51917820939a44b05818a0a59e3
Parsa Abedi <parsaab03@gmail.com>
2026-05-17 00:12:15 +0330

    feat: added auth routes

--- router/index.routes.js
+++ router/index.routes.js
@@ -1,6 +1,10 @@
 const { Router } = require("express");
 
+const { AuthRouters } = require("./auth.routes");
+
 const router = Router();
+
+router.use("/auth", AuthRouters)
 
 module.exports = {
   AllRouters: router,
