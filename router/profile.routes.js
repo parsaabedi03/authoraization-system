053ed3add37caa5f65a776cd7b8d650d8628ad7a
Parsa Abedi <parsaab03@gmail.com>
2026-05-18 13:12:40 +0330

    feat: create profile routes

---
+++ router/profile.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const { getProfile } = require("../controller/profile.controller");
+
+const router = Router();
+
+router.get("/profile", getProfile);
+
+module.exports = {
+  ProfileRouters: router,
+};
