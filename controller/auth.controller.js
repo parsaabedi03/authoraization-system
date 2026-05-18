f09842e55e44517a14f339fd0c8ef9201548015e
Parsa Abedi <parsaab03@gmail.com>
2026-05-18 11:16:19 +0330

    feat: added hashedPassword

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,9 +1,14 @@
 const userModel = require("../module/user.module");
+const { hashPassword } = require("../utils/auth.util");
 
 async function register(req, res, next) {
   try {
     const { fullName, email, password } = req.body;
-    const user = await userModel.create({ fullName, email, password });
+    const user = await userModel.create({
+      fullName,
+      email,
+      password: hashPassword(password),
+    });
     res.send(user);
   } catch (error) {
     next(error);
