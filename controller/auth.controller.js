25782d4b5b59f1b4a426ef1566377ea91950e48b
Parsa Abedi <parsaab03@gmail.com>
2026-05-18 12:00:23 +0330

    feat: added signToken

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,9 @@
 const userModel = require("../module/user.module");
-const { hashPassword, comparePassword } = require("../utils/auth.util");
+const {
+  hashPassword,
+  comparePassword,
+  signToken,
+} = require("../utils/auth.util");
 
 async function register(req, res, next) {
   try {
@@ -14,13 +18,15 @@ async function register(req, res, next) {
     next(error);
   }
 }
-function login(req, res, next) {
+async function login(req, res, next) {
   try {
     const { email, password } = req.body;
-    const user = userModel.findOne({ email });
+    const user = await userModel.findOne({ email });
     if (!user) throw { status: 404, message: "Not Found User" };
+
     if (comparePassword(password, user.password)) {
-      res.send("login system");
+      const token = signToken({ id: user._id, email: user.email });
+      res.send({ token, message: "login successfully" });
     } else {
       throw {
         status: 400,
