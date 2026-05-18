2d4edaddec04955a775366b434c2f0f64a31d801
Parsa Abedi <parsaab03@gmail.com>
2026-05-18 13:13:05 +0330

    feat: added profile routes

--- router/index.routes.js
+++ router/index.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 
 const { AuthRouters } = require("./auth.routes");
+const { ProfileRouters } = require("./profile.routes");
+const checkAuth = require("../middleware/check-auth");
 
 const router = Router();
 
-router.use("/auth", AuthRouters)
+router.use("/auth", AuthRouters);
+router.use("/user", checkAuth, ProfileRouters);
 
 module.exports = {
   AllRouters: router,
